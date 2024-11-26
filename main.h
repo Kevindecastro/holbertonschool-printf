e71e03c5bf8e7a6436f73bf0fa2ed5729fb3a1c5
Elisea 76 <9823@holbertonstudents.com>
2024-11-26 10:43:54 +0100

    Function & Prototype

--- main.h
+++ main.h
@@ -1,1 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdint.h>
+
 int _printf(const char *format, ...);
+int print_number(int n);
+int print_unsigned_number(unsigned int n);
+int print_octal(unsigned int n);
+int print_hexadecimal(unsigned int n, int uppercase);
+int print_pointer(uintptr_t n);
+int print_string(char *str);
+
+
+int _printf(const char *format, ...);
+
+#endif
