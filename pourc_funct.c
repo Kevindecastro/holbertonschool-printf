7125f9b43ea061535f5d2cecf6d5b8d1f23a72a6
Noam72T <9823@holbertonstudents.com>
2024-11-29 10:45:59 +0100

    Percentage Function

---
+++ pourc_funct.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_perc - Function Affiche le pourcentage.
+ * @list: la liste qui contient les arguments a passer.
+ * Return: 1 (Succes) .
+ */
+int print_perc(va_list list)
+{
+    (void)list;  
+    _putchar('%');  
+    return (1);  
+}
