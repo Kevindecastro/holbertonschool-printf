08d8ccce75a9ada0bd5156378782119341b3250a
Noam72T <9823@holbertonstudents.com>
2024-11-29 10:46:30 +0100

    String Function

---
+++ string_funct.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_string - Function Affiche une chaine de caratères.
+ * @list: la liste qui contient les arguments a passer.
+ * Return: Nombre total.
+ */
+int print_string(va_list list)
+{
+    char *str = va_arg(list, char *);
+    int count = 0;
+
+    if (str == NULL)  
+        str = "(null)";
+
+   
+    while (*str)
+    {
+        _putchar(*str);
+        str++;
+        count++;
+    }
+
+    return (count);
+}
