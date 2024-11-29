b17eec2e7092a1915f2cf1991a398e27e6297694
Noam72T <9823@holbertonstudents.com>
2024-11-29 15:17:30 +0100

    Prototype & Functions

--- main.h
+++ main.h
@@ -2,21 +2,12 @@
 #define MAIN_H
 
 #include <stdarg.h>
+#include <stddef.h>
 
-typedef struct vtype
-{
-    char tp;                        /* Type de spécificateur */
-    void (*f)(va_list, char *, int *); /* Fonction correspondante */
-} vtype_t;
 
-/* Prototypes des fonctions */
 int _printf(const char *format, ...);
-void format_c(va_list valist, char *buffer, int *index);
-void format_s(va_list valist, char *buffer, int *index);
-void format_d(va_list valist, char *buffer, int *index);
-void format_u(va_list valist, char *buffer, int *index);
-void format_perc(va_list valist, char *buffer, int *index);
-void _write_buffer(char *buffer, int *index);
-void reset_buffer(char *buffer);
+int print_char(char c);
+int print_string(const char *str);
+int print_number(int n);
 
-#endif /* MAIN_H */
+#endif 
