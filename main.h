26cd9be6c15f9dc519ad11630d107324615e8bb4
Noam72T <9823@holbertonstudents.com>
2024-11-29 10:41:39 +0100

    Main for functions

--- main.h
+++ main.h
@@ -1,1 +1,33 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <unistd.h>
+#include <stddef.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+
+/**
+* struct checker -Strucure du checker pour les functions demander
+* @type: Pointers du caractère string
+* @func_print: Pointer de la function d'affichage
+*/
+
+
+
+typedef struct checker
+{
+	char *type;
+
+	int (*func_print)(va_list list);
+} check_h;
+
+int _putchar(char c);
 int _printf(const char *format, ...);
+int print_char(va_list list);
+int print_string(va_list list);
+int print_perc(va_list list);
+int print_deci(va_list list);
+#endif 
