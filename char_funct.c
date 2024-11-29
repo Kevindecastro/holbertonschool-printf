afad3e8cd82a7a1929a66f43eff989554bba7982
Noam72T <9823@holbertonstudents.com>
2024-11-29 11:11:47 +0100

    Character Function

--- char_funct.c
+++ char_funct.c
@@ -1,13 +1,11 @@
 #include "main.h"
-
 /**
- * print_char - Fonction affiche un caractere.
- * @list: la liste qui contient les arguments a passer.
- * Return: 1(Succes).
- */
-
+* print_char - Fonction affiche un caractere.
+* @list: la liste qui contient les arguments a passer.
+* Return: 1(Succes).
+*/
 int print_char(va_list list)
 {
-    _putchar(va_arg(list, int));  
-    return (1);  
+_putchar(va_arg(list, int));
+return (1);
 }
