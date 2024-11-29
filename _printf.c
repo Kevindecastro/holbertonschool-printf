252d093a3122d5d302b5f71650cbbb80e974a96d
Noam72T <9823@holbertonstudents.com>
2024-11-29 11:16:49 +0100

    Update Function Printf

--- _printf.c
+++ _printf.c
@@ -4,61 +4,44 @@
  * @format: Chaine contenant le format a afficher.
  * Return: Nombre total à afficher
  */
-int _printf(const char *format, ...)
+int _printf(const char *format, ...) 
 {
-    /**Declare mes variables*/
-    int i = 0, count = 0; 
-    va_list list;
-
-    /**Declare mes Specificateurs*/
-    check_h spec[] = {
-        {"c", print_char},
-        {"s", print_string},
-        {"%", print_perc},
-        {"d", print_deci},
-        {"i", print_deci},
-        {NULL, NULL},
-    };
-
-    /** Initialisation de la list et condition Null */
-    va_start(list, format);
-    
-    if (format == NULL) 
-        return (-1);
-    /** Parcours de la chaines */
-    while (format[i] != '\0')
-    { 
-        if (format[i] == '%')
-        {
-            i++;
-            int found = 0;
-            int j;
-            /** Itere chaque elements du tableau et compare le char apres % */
-            for (j = 0; spec[j].type; j++)
-            {
-                if (format[i] == *spec[j].type)
-                {
-                    /** Imprime le nombre de char */
-                    count += spec[j].func_print(list);
-                    found = 1;
-                    break;
-                }
-            }
-        /** Si rien trouver */
-            if (!found) {
-                _putchar('%');
-                _putchar(format[i]);
-                /** Ajoute 2 au compteur pour les 2_putchar*/
-                count += 2;
-            }
-        } else {
-        /** Si rien on Incremente*/
-            _putchar(format[i]);
-            count++;
-        }
-        i++;
-    }
-/** Fin et liberations de l'espaces*/
-    va_end(list);
-    return (count);
-    }
+int i = 0, count = 0; 
+va_list list;
+check_h spec[] = {
+{"c", print_char}, {"s", print_string}, {"%", print_perc},
+{"d", print_deci}, {"i", print_deci}, {NULL, NULL},
+};
+va_start(list, format);
+if (format == NULL) 
+return (-1);
+while (format[i] != '\0') 
+{ 
+if (format[i] == '%') {
+i++;
+int found = 0;
+int j;
+for (j = 0; spec[j].type; j++) 
+{
+if (format[i] == *spec[j].type) 
+{
+count += spec[j].func_print(list);
+found = 1;
+break;
+}
+}
+if (!found) 
+{
+_putchar('%');
+_putchar(format[i]);
+count += 2;
+}
+} else {
+_putchar(format[i]);
+count++;
+}
+i++;
+}
+va_end(list);
+return (count);
+}
