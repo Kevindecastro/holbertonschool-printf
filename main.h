ee91209bb17717006355803efdc222cba5764830
Noam72T <9823@holbertonstudents.com>
2024-11-29 14:44:51 +0000

    Function & Prototype

--- main.h
+++ main.h
@@ -4,10 +4,9 @@
 #include <stdarg.h>
 #include <stddef.h>
 
-
 int _printf(const char *format, ...);
 int print_char(char c);
 int print_string(const char *str);
 int print_number(int n);
 
-#endif 
+#endif
