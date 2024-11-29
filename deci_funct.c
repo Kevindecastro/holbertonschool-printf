898eb6aebbcc5948902faf9ca2f09dcb7eda0216
Noam72T <9823@holbertonstudents.com>
2024-11-29 11:12:39 +0100

    Decimal Function

--- deci_funct.c
+++ deci_funct.c
@@ -7,35 +7,31 @@
  */
 int print_deci(va_list list)
 {
-    int n = va_arg(list, int);
-    unsigned int num = (n < 0) ? -n : n; 
-    int count = 0;
+int n = va_arg(list, int);
+unsigned int num = (n < 0) ? -n : n; 
+int count = 0;
 
-    if (n < 0)  
-    {
-        _putchar('-');
-        count++;
-    }
-
-    if (num == 0)  
-    {
-        _putchar('0');
-        return count + 1;
-    }
-
-    char buffer[10];
-    int i = 0;
-    while (num)
-    {
-        buffer[i++] = (num % 10) + '0';
-        num /= 10;
-    }
-
-    while (--i >= 0)  
-    {
-        _putchar(buffer[i]);
-        count++;
-    }
-
-    return count;
+if (n < 0)  
+{
+_putchar('-');
+count++;
+}
+if (num == 0)  
+{
+_putchar('0');
+return count + 1;
+}
+char buffer[10];
+int i = 0;
+while (num)
+{
+buffer[i++] = (num % 10) + '0';
+num /= 10;
+}
+while (--i >= 0)  
+{
+_putchar(buffer[i]);
+count++;
+}
+return count;
 }
