94494ea04468b6f5444135b2f047e072205c2990
Noam72T <9823@holbertonstudents.com>
2024-11-29 13:50:30 +0100

    Main For prototype

--- main.h
+++ main.h
@@ -1,33 +1,22 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <unistd.h>
-#include <stddef.h>
 #include <stdarg.h>
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
 
-
-/**
-* struct checker -Strucure du checker pour les functions demander
-* @type: Pointers du caractère string
-* @func_print: Pointer de la function d'affichage
-*/
-
-
-
-typedef struct checker
+typedef struct vtype
 {
-	char *type;
+    char tp;                        /* Type de spécificateur */
+    void (*f)(va_list, char *, int *); /* Fonction correspondante */
+} vtype_t;
 
-	int (*func_print)(va_list list);
-} check_h;
-
-int _putchar(char c);
+/* Prototypes des fonctions */
 int _printf(const char *format, ...);
-int print_char(va_list list);
-int print_string(va_list list);
-int print_perc(va_list list);
-int print_deci(va_list list);
-#endif 
+void format_c(va_list valist, char *buffer, int *index);
+void format_s(va_list valist, char *buffer, int *index);
+void format_d(va_list valist, char *buffer, int *index);
+void format_u(va_list valist, char *buffer, int *index);
+void format_perc(va_list valist, char *buffer, int *index);
+void _write_buffer(char *buffer, int *index);
+void reset_buffer(char *buffer);
+
+#endif /* MAIN_H */
